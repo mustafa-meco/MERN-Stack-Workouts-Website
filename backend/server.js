9df51e3d6d46ea8afc5d15e302ce42ee178be959
Mustafa Ghoneim <musgamal05@gmail.com>
2024-01-15 16:00:56 +0200

    express App Setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+
+// Create express app
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my application." });
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("Server is listening on port", process.env.PORT);
+});
