19b5015745b6169a01fb075e46eff174df95111b
Mustafa Ghoneim <musgamal05@gmail.com>
2024-01-15 16:40:55 +0200

    Models & Schema

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Workout = require("../models/workoutModel");
 
 const router = express.Router();
 
@@ -13,8 +14,14 @@ router.get("/:id", (req, res) => {
 });
 
 // POST one workout
-router.post("/", (req, res) => {
-  res.json({ message: "POST one workout" });
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(201).json(workout);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 // DELETE one workout
