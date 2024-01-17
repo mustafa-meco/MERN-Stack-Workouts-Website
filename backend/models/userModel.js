f7d7c287d3376ac2f4ca47a53da9d445e8935545
Mustafa Ghoneim <musgamal05@gmail.com>
2024-01-17 19:07:10 +0200

    Sighning Up & Hashing Passwords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,16 +1,38 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
   },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-});
+  { timestamps: true }
+);
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("User already exists");
+  }
+
+  // mypassword123 => 123456
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 const User = mongoose.model("User", userSchema);
 
